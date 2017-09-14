f4cc063672309ae50914bc3acd9f217967789177
Majestic3691 <mtr3691@hotmail.com>
2017-09-14 17:02:51 -0500

    Initial commit

---
+++ InstallConfigureEnv.ps1
@@ -0,0 +1,11 @@
+﻿Get-ExecutionPolicy
+Set-ExecutionPolicy AllSigned; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco -?
+choco install git -y
+choco install openssh -y -params "\SSHAgentFeature"
+choco install nodejs npm -y
+npm
+choco install atom -y
+choco install python2 -y
+choco install awscli -y
+choco install unzip Less -y
