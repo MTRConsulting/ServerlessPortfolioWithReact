86d36c39687c6513367b2908e029f9bbe55ff8bf
Majestic3691 <mtr3691@hotmail.com>
2017-09-20 22:56:20 -0500

    Fixed error on print. Changed % to .format

--- code/upload-portfolio-lambda.py
+++ code/upload-portfolio-lambda.py
@@ -18,20 +18,20 @@ the_zipfile = ''
 portfolio_bucket = s3.Bucket('portfolio.michaelryanitprofessional.net')
 build_bucket = s3.Bucket('portfoliobuild.michaelryanitprofessional.net')
 
-print ("Bucket: {}" %(portfolio_bucket.name))
+print ("Bucket: {}" .format(portfolio_bucket.name))
 for obj in portfolio_bucket.objects.all():
-  print ("File: {}" %(obj.key))
+  print ("File: {}" .format(obj.key))
 
-print ("Bucket: {}" %(build_bucket.name))
+print ("Bucket: {}" .format(build_bucket.name))
 for obj in build_bucket.objects.all():
   the_zipfile = obj.key
-  print ("File: {}" %(obj.key))
+  print ("File: {}" .format(obj.key))
 
 portfolio_bucket.download_file('index.html', 'c:\scratch\labbuild\index.html')
 build_bucket.download_file('portfoliobuild.zip', 'c:\scratch\labbuild\portfoliobuild.zip')
-print ("Uploading contents of zip file |{}|" %(the_zipfile) )
-print ("  from bucket |{}| created on |{}|" %(portfolio_bucket.name, portfolio_bucket.creation_date) )
-print ("  to bucket |{}| created on |{}| at |{}|" %(build_bucket.name, build_bucket.creation_date, datetime.now().strftime("%Y-%m-%d %H:%M:%S")) )
+print ("Uploading contents of zip file |{}|" .format(the_zipfile) )
+print ("  from bucket |{}| created on |{}|" .format(portfolio_bucket.name, portfolio_bucket.creation_date) )
+print ("  to bucket |{}| created on |{}| at |{}|" .format(build_bucket.name, build_bucket.creation_date, datetime.now().strftime("%Y-%m-%d %H:%M:%S")) )
 #print ("Uploading contents of zip file |%s| from bucket |%s| created on |%s| to bucket |%s| created on |%s| at |%s|" %(the_zipfile) %(portfolio_bucket.name) %(portfolio_bucket.creation_date) %(build_bucket.name) %(build_bucket.creation_date) %(datetime.now().strftime("%Y-%m-%d %H:%M:%S")) )
 portfolio_zip = io.BytesIO()
 build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
@@ -41,4 +41,4 @@ with zipfile.ZipFile(portfolio_zip) as myzip:
     obj = myzip.open(nm)
     portfolio_bucket.upload_fileobj(obj, nm)
     portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-    print("Uploading file: {}" %(nm))
+    print("Uploading file: {}" .format(nm))
