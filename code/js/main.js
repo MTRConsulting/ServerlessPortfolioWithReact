ddfa4bb99fb360006bda980c7d889e156ce30437
Michael Ryan <mtr3691@yahoo.com>
2017-10-31 12:38:51 -0500

    Added 3 work examples. 2 new images.

--- code/js/main.js
+++ code/js/main.js
@@ -4,9 +4,9 @@ import ExampleWork from './example-work'
 
 const myWork = [
   {
-    'title': 'Work Example',
-    'href': 'https://example.com',
-    'desc': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    'title': 'Javascript Test Code Example',
+    'href': 'https://github.com/MTRConsulting/ServerlessPortfolioWithReact/blob/master/code/__tests__/test-example-work.js',
+    'desc': 'Test code for this portfolio web site using React (Facebook) & Enzyme (Airbnb).',
     'image': {
       'desc': 'example screenshot of a project involving code',
       'src': 'images/example1.png',
@@ -14,24 +14,23 @@ const myWork = [
     }
   },
   {
-    'title': 'Portfolio Boilerplate',
-    'href': 'https://example.com',
-    'desc': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    'title': 'AWS Lambda function to build/deploy site to S3',
+    'href': 'https://github.com/MTRConsulting/ServerlessPortfolioWithReact/blob/master/code/upload-portfolio-lambda.py',
+    'desc': 'This example demonstrates the use of Serverless architecture to build and deploy a static web site to AWS S3.',
     'image': {
       'desc': 'A Serverless Portfolio',
-      'src': 'images/example2.png',
+      'src': 'images/serverless-portfolio-with-react.png',
       'comment': ''
     }
   },
   {
-    'title': 'Work Example',
-    'href': 'https://example.com',
-    'desc': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    'title': '.Net Parallel Example',
+    'href': 'https://gitlab.com/MajesticOpen/ContractManager/blob/master/ContractManager/ContractManager/ContractService.cs',
+    'desc': 'Sample of code using the Parallel .NET library. The context is processing scanned contracts with an execution condition based on processor count.',
     'image': {
-      'desc': 'example screenshot of a project involving cats',
-      'src': 'images/example3.png',
-      'comment': `“Bengal cat” by roberto shabs is licensed under CC BY 2.0
-                   https://www.flickr.com/photos/37287295@N00/2540855181`
+      'desc': 'example screenshot of parallel For loop',
+      'src': 'images/ParallelExample3.png',
+      'comment': `Code from a client engagement`
     }
   }
 ]
